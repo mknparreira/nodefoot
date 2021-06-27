31b98c62d1eef3bd7914952d34ea3a0c300325ef
Maikon Benevides <maikon.benevides@decskill.com>
2021-06-27 14:02:20 +0100

    Fixed a bug for retrive sequelize object

--- src/config/Database.config.js
+++ src/config/Database.config.js
@@ -4,27 +4,23 @@ const Sequelize = require('sequelize');
 const globalConfig = require('config');
 
 class Database {
-  constructor() {
-    this.config = globalConfig;
-  }
-
-  static _connection() {
-    const databaseConfig = this.config.get('Database');
+  _connection() {
+    const databaseConfig = globalConfig.get('Database');
 
     return new Sequelize(databaseConfig.database,
       databaseConfig.username, databaseConfig.password, databaseConfig);
   }
 
-  static _filesystem() {
+  _filesystem() {
     const db = {};
     const basename = path.basename(__filename);
-    const sequelize = this.constructor._connection();
+    const sequelize = this._connection();
 
     fs
       .readdirSync(__dirname)
       .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
       .forEach((file) => {
-        const model = sequelize.import(path.join(__dirname, file));
+        const model = require(path.join(__dirname, file));
         db[model.name] = model;
       });
 
